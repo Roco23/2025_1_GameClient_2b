fcc4075233c2ea212a588d31d3dc917663f1e36e
croco <croco1120@naver.com>
2025-04-21 11:08:16 +0900

    [Feat] 카드 매니저 제작 & 마나 개념 추가

--- Assets/CardGame/CaracterStats.cs
+++ Assets/CardGame/CaracterStats.cs
@@ -14,11 +14,18 @@ public class CaracterStats : MonoBehaviour
     public Slider healthBar;
     public TextMeshProUGUI healthText;
 
+    //새로 추가 되는 변수
+    public int maxMana = 10;
+    public int currentMana;
+    public Slider manaBar;
+    public TextMeshProUGUI manaText;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentMana = maxMana;
+        UpdateUI();
     }
 
    public void TakeDamage(int damage)  //매개변수 데미지 
@@ -28,5 +35,48 @@ public class CaracterStats : MonoBehaviour
     public void Heal(int amount)
     {
         currentHealth += amount;
+    }
+
+    public void UseMana(int amount)
+    {
+        currentMana -= amount;
+        if (currentMana < 0)
+        {
+            currentMana = 0;
+        }
+        UpdateUI();
+    }
+
+
+    public void GainMana(int amount)
+    {
+        currentMana += amount;
+        if(currentMana > maxMana)
+        {
+            currentMana = maxMana;
+        }
+        UpdateUI();
+    }
+    private void UpdateUI()
+    {
+        if(healthBar != null)
+        {
+            healthBar.value = (float)currentHealth / maxHealth;
+        }
+
+        if(healthText != null)
+        {
+            healthText.text = $"{currentHealth} / {maxHealth}";
+        }
+
+        if(manaBar != null)
+        {
+            manaBar.value = (float)currentMana / maxMana;
+        }    
+
+        if(manaText != null)
+        {
+            manaText.text = $" {currentMana} / {maxMana}";
+        }
     }
 }
